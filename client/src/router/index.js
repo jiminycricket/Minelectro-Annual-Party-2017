acd56a180054ab1fb71bea86440f7c7704d55f11
jiminycricket <jiminy.upon.star@gmail.com>
2017-09-25 14:27:59 +0800

    update script

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Home from '@/components/page/Home';
+import Vote from '@/components/page/Vote';
+import Success from '@/components/page/Success';
 
 Vue.use(Router);
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/vote',
+      name: 'Vote',
+      component: Vote,
+    },
+    {
+      path: '/success',
+      name: 'Success',
+      component: Success,
     },
   ],
 });
