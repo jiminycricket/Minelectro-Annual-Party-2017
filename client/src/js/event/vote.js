1da8439ad19cf44415c90b068e2e163d0b3fec5f
jiminycricket <jiminy.upon.star@gmail.com>
2017-09-27 21:43:17 +0800

    vote modal

--- client/src/js/event/vote.js
+++ client/src/js/event/vote.js
@@ -2,17 +2,36 @@ import { questionsArray } from '../const/questions';
 import { postVotes } from '../service/api';
 import { toggleSuccessModal } from './success'
 const $ = window.$;
-
+const singleSelect = ["question_1", "question_2", "question_4", "question_5", "question_6"];
+const fourSlectTwo = ["question_3"];
 const init = () => {
-  $('#voteModal').on('show.bs.modal', (e) => {
-    questionsArray.forEach((question) => {
-      optionEvent(question);
+  // $('#voteModal').on('show.bs.modal', (e) => {
+    singleSelect.forEach((question) => {
+      optionSingleEvent(question);
+    });
+    fourSlectTwo.forEach((question) => {
+      optionfourSlectTwoEvent(question);
     });
     submit();
+  // });
+}
+
+const optionfourSlectTwoEvent = (question) => {
+  const $options = $(`.option[data-vote-question=${question}]`);
+  $options.on('click', function (e) {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      return;
+    }
+    if ($(`.option[data-vote-question=${question}].active`).length >= 2) {
+      return;
+    } else {
+      $(this).addClass("active");
+    }
   });
 }
 
-const optionEvent = (question) => {
+const optionSingleEvent = (question) => {
   const $options = $(`.option[data-vote-question=${question}]`);
   $options.on('click', function () {
     $options.removeClass("active");
@@ -31,6 +50,7 @@ const toggleVoteModal = () => {
 const submit = () => {
   $('.js-send-votes').on('click', () => {
     offSubmit();
+    questionsArray
     const voteOptions = questionsArray.map((question) => {
       return $(`.option[data-vote-question=${question}].active`).attr('data-option-id');
     }).filter((option) => {
