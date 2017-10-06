5e2e041793d63b47547eb632eebc9ea1250f20d7
jiminycricket <jiminy.upon.star@gmail.com>
2017-10-06 21:53:25 +0800

    animate

--- index/src/js/waypoint/callToAction.js
+++ index/src/js/waypoint/callToAction.js
@@ -9,6 +9,19 @@ const init = () => {
   $('.tickets').waypoint({
     handler: function (direction) {
       $(".js-call-to-action").toggleClass("active", direction === 'up');
+      $(".background").removeClass("active");
+      if(direction === 'down') {
+        setTimeout(function() {
+          $(".background").addClass("active");
+        }, 100);
+      }
+    },
+    offset: '50%'
+  });
+
+  $('.intro').waypoint({
+    handler: function (direction) {
+      $(".scene").toggleClass("hiding", direction === 'down');
     },
     offset: '50%'
   });
--- index/src/main.js
+++ index/src/main.js
@@ -28,11 +28,11 @@ $(document).ready(() => {
   var scene2 = document.getElementById('scene2');
   var scene3 = document.getElementById('scene3');
   var scene4 = document.getElementById('scene4');
-  var parallaxInstance = new Parallax(scene);
-  var parallaxInstance = new Parallax(scene1);
-  var parallaxInstance = new Parallax(scene2);
-  var parallaxInstance = new Parallax(scene3);
-  var parallaxInstance = new Parallax(scene4);
+  // var parallaxInstance = new Parallax(scene);
+  // var parallaxInstance = new Parallax(scene1);
+  // var parallaxInstance = new Parallax(scene2);
+  // var parallaxInstance = new Parallax(scene3);
+  // var parallaxInstance = new Parallax(scene4);
   ga.init();
   $(".top-cover").addClass("end");
   setTimeout(function() {
