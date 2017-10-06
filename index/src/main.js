a09222aa86c5d438dd8c00130716bcb87a1bee3a
jiminycricket <jiminy.upon.star@gmail.com>
2017-10-06 15:46:28 +0800

    parallax

--- index/src/main.js
+++ index/src/main.js
@@ -6,6 +6,8 @@ import waypointBackToTop from './js/waypoint/backToTop';
 import callToAction from './js/waypoint/callToAction';
 import backToTop from './js/service/backToTop';
 import ga from './js/event/ga';
+import Parallax from 'parallax-js';
+
 const $ = window.$;
 
 window.votes = window.votes ? JSON.parse(window.votes) : votes;
@@ -21,9 +23,20 @@ $(document).ready(() => {
   waypointBackToTop.init();
   backToTop.init();
   callToAction.init();
+  var scene = document.getElementById('scene');
+  var scene1 = document.getElementById('scene1');
+  var scene2 = document.getElementById('scene2');
+  var scene3 = document.getElementById('scene3');
+  var scene4 = document.getElementById('scene4');
+  var parallaxInstance = new Parallax(scene);
+  var parallaxInstance = new Parallax(scene1);
+  var parallaxInstance = new Parallax(scene2);
+  var parallaxInstance = new Parallax(scene3);
+  var parallaxInstance = new Parallax(scene4);
   ga.init();
   $(".top-cover").addClass("end");
   setTimeout(function() {
     $(".top-cover").remove();
+    $(".background").addClass("active");
   }, 2000);
 })
